94070f50082debe94e509b711a5ce7c530e911b4
Fabio Carvalho <fabio@gwnet.com.br>
2025-01-12 14:21:43 -0300

    Implemented test on WikiLargestCompaniesService

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,30 +5,9 @@ namespace Tests;
 
 use Laravel\Lumen\Testing\DatabaseTransactions;
 use Laravel\Lumen\Testing\TestCase as BaseTestCase;
-use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use DatabaseTransactions;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-        $this->resetDatabase();
-    }
-
-    public function resetDatabase(): void
-    {
-        $tables = [
-            'test.capture_company',
-            'test.company'
-        ];
-
-        foreach ($tables as $full_table_name) {
-            DB::statement("
-                TRUNCATE TABLE {$full_table_name};
-            ");
-        }
-    }
 }
